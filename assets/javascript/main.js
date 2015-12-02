caa09acd03fb3f24a9dfde590f2e9993aaf85a67
Diana Arteaga <diana.arteaga33@gmail.com>
2015-12-02 01:19:24 -0800

    Fixed some formatting issues

---
+++ assets/javascript/main.js
@@ -0,0 +1,19 @@
+var $ = jQuery;
+
+$(document).ready(function(){
+	$(".js-femtech").click(function(){
+		$(".femtech").fadeIn();
+	}) 
+
+	$(".js-bevisible").click(function(){
+		$(".bevisible").fadeIn();
+		
+	}) 
+});
+
+
+
+
+
+
+
